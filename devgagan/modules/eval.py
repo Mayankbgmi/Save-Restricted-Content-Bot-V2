38d0109ff9507888a64178bc25d019a3ceb8cde6
Mayank Jaat <jatmayankjatm25@gmail.com>
2024-08-07 14:17:42 +0530

    Update eval.py

--- devgagan/modules/eval.py
+++ devgagan/modules/eval.py
@@ -40,7 +40,7 @@ async def edit_or_reply(msg, **kwargs):
 )
 async def executor(client, message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="<b>ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴɴᴀ ᴇxᴇᴄᴜᴛᴇ ʙᴀʙʏ ?</b>")
+        return await edit_or_reply(message, text="<b>ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴɴᴀ ᴇxᴇᴄᴜᴛᴇ ?</b>")
     try:
         cmd = message.text.split(" ", maxsplit=1)[1]
     except IndexError:
