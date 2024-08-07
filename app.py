18f9590186da6cd02d08042ef9fefab27bc437fb
Mayank Jaat <jatmayankjatm25@gmail.com>
2024-08-07 14:47:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return """
     <center>
-        <img src="/static/livebot.pg" style="border-radius: 2px;"/>/>
+        <img src="/livebot.pg" style="border-radius: 2px;"/>/>
     </center>
     <style>
         body {
